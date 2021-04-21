3ff9b879ed80e61edeb45c943d570ea226890d6e
lbunderwood <luke.b.underwood@gmail.com>
2021-04-21 09:55:04 -0800

    Assignment 7 complete

--- Assignment7/collcount.pl
+++ Assignment7/collcount.pl
@@ -6,3 +6,28 @@
 % contains predicate collcount, which correlates a number with the number of
 % iterations of the collatz function required to reduce the number to 1
 
+% counter/3
+% counter(+n, +s, ?i) 
+% i is the number of iterations that have already been performed
+% n is the number initially given to collcount
+% s is a starting point for i, should always be set to 0 by collcount
+counter(1, S, I) :- I is S.
+counter(N, S, I) :- 
+                N > 1,
+                N mod 2 =:= 0,
+                S1 is S + 1,
+                N1 is N / 2,
+                counter(N1, S1, I).
+counter(N, S, I) :- 
+                N > 1,
+                mod(N, 2) > 0,
+                S1 is S + 1,
+                N1 is N * 3 + 1,
+                counter(N1, S1, I).
+
+
+% collcount/2
+% collcount(+n, ?c) c is iterations of collatz for number n
+collcount(N, C) :- 
+                N >= 1,
+                counter(N, 0, C).
